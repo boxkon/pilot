e80fbbe990b74a56bcd24ae2a9b0aadc99558ff2
boxkon <ghkdxo14@gmail.com>
2024-06-27 13:34:30 +0900

    fix bug

--- common/params.cc
+++ common/params.cc
@@ -220,7 +220,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"UseExternalNaviRoutes", PERSISTENT},
     {"DriverCameraOnReverse", PERSISTENT},
     {"CanFdHda2", PERSISTENT},
-    {"SyncSetSpeedWhileGas", PERSISTENT}
+    {"SyncSetSpeedWhileGas", PERSISTENT},
 
     {"LaneChangeEnabled", PERSISTENT}, //boxkon
     {"AutoLaneChangeEnabled", PERSISTENT}, //boxkon
